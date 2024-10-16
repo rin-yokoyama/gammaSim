2b93645a2c0c19c203f3afd42d96672ea866ad7b
Rin Yokoyama <yokoyama@cns.s.u-tokyo.ac.jp>
2024-10-16 13:50:19 +0900

    152eu test run

--- include/ExpConstants.hh
+++ include/ExpConstants.hh
@@ -6,7 +6,7 @@
 
 namespace B1
 {
-    const G4String kSourceFile = "./gamma_definitions/152Eu(updated).txt";
+    const G4String kSourceFile = "./gamma_definitions/update152Eu.txt";
     const G4double kWorldSize = 200. * cm;
     const G4double kGeRadius = 57.1 * mm;
     const G4double kGeLength = 54.6 * mm;
