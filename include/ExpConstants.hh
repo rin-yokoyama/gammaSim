68ccb6a45c3a5d2228a09012848db261c47f3be1
Rin Yokoyama <yokoyama@cns.s.u-tokyo.ac.jp>
2024-12-20 11:29:09 +0900

    allinc2co reset

--- include/ExpConstants.hh
+++ include/ExpConstants.hh
@@ -22,7 +22,7 @@ namespace B1
     static G4RotationMatrix kDetRotation(0, -90 * deg, 0);
     static G4ThreeVector kDetPosition(0, -(kWindowPos.getZ() + kWindowThickness), 0);
     static G4RotationMatrix kSampleRotation(0, 0, 0);
-    static G4ThreeVector kSamplePosition(100. * mm, (kSourceSizeY / 2.) + (10. * mm), 100. * mm);
+    static G4ThreeVector kSamplePosition(50. * mm, (kSourceSizeY / 2.) + (10. * mm), 50. * mm);
 }
 
 #endif
