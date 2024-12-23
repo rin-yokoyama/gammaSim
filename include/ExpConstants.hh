0e12cea4713afa87dde80be33d20ec7556411a3b
Rin Yokoyama <yokoyama@cns.s.u-tokyo.ac.jp>
2024-12-23 15:10:03 +0900

    x=15mm,z=10mm

--- include/ExpConstants.hh
+++ include/ExpConstants.hh
@@ -22,7 +22,7 @@ namespace B1
     static G4RotationMatrix kDetRotation(0, -90 * deg, 0);
     static G4ThreeVector kDetPosition(0, -(kWindowPos.getZ() + kWindowThickness), 0);
     static G4RotationMatrix kSampleRotation(0, 0, 0);
-    static G4ThreeVector kSamplePosition(20. * mm, (kSourceSizeY / 2.), 10. * mm);
+    static G4ThreeVector kSamplePosition(15. * mm, (kSourceSizeY / 2.), 10. * mm);
     static const bool k60CoGammaAngularCorrelation = true;
 }
 
